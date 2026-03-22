5d5f12e3679ee6a260924ab327a24c5add44c901
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-22 16:55:24 +0200

    Add homepage route

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Aquavaal Booking System is Live 🎣")
 
 urlpatterns = [
-    path('', include('bookings.urls')),
+    path('', home),   # 👈 THIS LINE FIXES YOUR ISSUE
     path('admin/', admin.site.urls),
 ]
