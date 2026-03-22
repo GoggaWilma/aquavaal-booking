60c6b8e8789f3cbdc9852eb273ddb06926dcc192
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-22 19:32:41 +0200

    Fix urls routing

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -1,11 +1,12 @@
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Aquavaal Booking System is Live 🎣")
 
 urlpatterns = [
-    path('', home),   # 👈 THIS LINE FIXES YOUR ISSUE
     path('admin/', admin.site.urls),
+    path('', include('bookings.urls')),  # 👈 main app
 ]
