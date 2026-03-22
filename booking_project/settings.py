c09054eca4c0a522b10b18720fb7021a2b798525
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-22 22:16:17 +0200

    Switch to PostgreSQL database

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'booking_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
