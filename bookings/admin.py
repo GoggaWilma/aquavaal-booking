9c5078fcd900da9fa956b549d1721957136fa912
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-23 13:02:38 +0200

    Add safe booking admin display

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "arrival_datetime", "departure_datetime", "created_at")
