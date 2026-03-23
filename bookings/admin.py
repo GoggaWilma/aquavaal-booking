5a89eb855d87ca75c7d83496bea7aa8eae6a453e
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-23 14:29:24 +0200

    Add stand assignment inline

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, BookingStand
+
+
+class BookingStandInline(admin.TabularInline):
+    model = BookingStand
+    extra = 1  # shows empty row to add stands
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "arrival_datetime", "departure_datetime", "created_at")
+    list_display = (
+        "id",
+        "user",
+        "payment_status",
+        "attendance_status",
+        "arrival_datetime",
+    )
+
+    inlines = [BookingStandInline]
