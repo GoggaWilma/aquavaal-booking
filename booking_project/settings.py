e76e9fc024104af713784f6d412463fd4b62ec05
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-30 18:03:21 +0200

    Error 500

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -71,11 +71,7 @@ ROOT_URLCONF = 'booking_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-
-        import os
-
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
