dc4e67acc3eade14803b9dbbefbcf389acd67ec9
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-03-30 12:55:58 +0200

    Fix user creation and profile signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,93 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
-from .resources import UserResource
+from .models import CustomUser, Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    extra = 0
+    fk_name = "user"
+
+    fields = (
+        "membership_type",
+        "owned_stand",
+        "surname",
+        "first_name",
+        "call_name",
+        "initials",
+        "ID_number",
+        "date_of_birth",
+        "savof_code",
+        "gender",
+        "membership_expiry_date",
+        "cell_number",
+        "house_number",
+        "street_name",
+        "suburb",
+        "city",
+        "province",
+        "postal_code",
+    )
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(ImportExportModelAdmin):
-    resource_class = UserResource
-    list_display = ("email", "is_active")
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    inlines = [ProfileInline]
+
+    list_display = ("email", "is_active", "is_staff", "is_superuser")
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "membership_type",
+        "owned_stand",
+        "membership_expiry_date",
+    )
+    search_fields = (
+        "user__email",
+        "surname",
+        "first_name",
+        "cell_number",
+    )
+    list_filter = ("membership_type", "gender")
--- accounts/models.py
+++ accounts/models.py
@@ -121,16 +121,4 @@ class Profile(models.Model):
         return f"{self.user.email} Profile"
 
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-
-@receiver(post_save, sender=CustomUser)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=CustomUser)
-def save_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'profile'):
-        instance.profile.save()
