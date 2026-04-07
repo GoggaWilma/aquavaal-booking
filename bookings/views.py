f145a954ff45fd4e211a73b9dc1382cb756e58d9
Wilma Engelbrecht <wilma.engelbrecht@gmail.com>
2026-04-07 18:19:26 +0200

    500 error

--- bookings/views.py
+++ bookings/views.py
@@ -119,19 +119,19 @@ def dashboard(request):
                             "name": bs.booking.display_name(),
                             "arrival": bs.booking.arrival_datetime.strftime("%d %b %Y"),
                             "departure": bs.booking.departure_datetime.strftime("%d %b %Y"),
-                    }
+                        }
 
-                    if existing:
-                        existing["bookings"].append(entry)
-                    else:
-                        pending_stands.append({
-                            "id": bs.stand.id,
-                            "number": bs.stand.number,
-                            "bookings": [entry],
-                            "name": entry["name"],
-                            "arrival": entry["arrival"],
-                            "departure": entry["departure"],
-                    })
+                        if existing:
+                            existing["bookings"].append(entry)
+                        else:
+                            pending_stands.append({
+                                "id": bs.stand.id,
+                                "number": bs.stand.number,
+                                "bookings": [entry],
+                                "name": entry["name"],
+                                "arrival": entry["arrival"],
+                                "departure": entry["departure"],
+                            })
 
                 blocked_ids = booked_stand_ids | unavailable_stand_ids | pending_stand_ids
 
